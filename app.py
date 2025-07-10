c1f02ef83fc0cfa54fb11482d6111f6b7795739b
rushi78441 <rushi78441@gmail.com>
2025-07-10 21:37:55 +0530

    ./

--- app.py
+++ app.py
@@ -4,6 +4,7 @@
 
 import os
 import warnings
+warnings.simplefilter("ignore")
 import sys
 
 import pandas as pd
@@ -53,6 +54,7 @@ if __name__ == "__main__":
     train_y = train[["quality"]]
     test_y = test[["quality"]]
 
+    ## psoitional arguments
     alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
     l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
 
